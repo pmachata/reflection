264d9ab76149332798824cb2617b74d97f20f010
Petr Machata <pmachata@redhat.com>
2011-09-16 19:03:07 +0200

    Add licensing
    - GPL, because elfutils are also GPL

--- refl.h
+++ refl.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2011 Petr Machata <pmachata@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _REFL_H_INCLUDED
+#define _REFL_H_INCLUDED
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -73,3 +93,5 @@ void *refl_object_cdata (struct refl_object *obj);
 #ifdef __cplusplus
 }
 #endif
+
+#endif//_REFL_H_INCLUDED
--- reflP.h
+++ reflP.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2011 Petr Machata <pmachata@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _REFLP_H_INCLUDED
+#define _REFLP_H_INCLUDED
+
 #include <elfutils/libdwfl.h>
 #include "refl.h"
 
@@ -63,3 +83,4 @@ int __refl_each_die (Dwfl_Module *module, Dwarf_Die *root, Dwarf_Die *ret,
 struct refl_object *__refl_object_begin (struct refl_type *type, void *data);
 struct refl_object *__refl_object_begin_inline (struct refl_type *type,
 						size_t size);
+#endif//_REFLP_H_INCLUDED
